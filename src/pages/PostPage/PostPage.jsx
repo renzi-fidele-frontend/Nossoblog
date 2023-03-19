120629a2d3cb0679b667206e52f6d688f7c2d2c3
Renzi Fidele <renzifidele2001@gmail.com>
2023-03-19 20:12:58 +0200

    Hora de avancar

---
+++ src/pages/PostPage/PostPage.jsx
@@ -0,0 +1,11 @@
+import styles from "./PostPage.module.css"
+
+const PostPage = () => {
+  return (
+    <div id={styles.container}>
+        <p>postPage</p>
+    </div>
+  )
+}
+
+export default PostPage
