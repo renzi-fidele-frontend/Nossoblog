9528a536b90ab9717137b861dc889a25cb1e9616
Renzi Fidele <renzifidele2001@gmail.com>
2023-03-23 00:20:40 +0200

    corrigido o problema de conversao da string para html

--- src/pages/PostPage/PostPage.jsx
+++ src/pages/PostPage/PostPage.jsx
@@ -2,17 +2,21 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { db } from "../../firebase/config";
 import styles from "./PostPage.module.css";
+import estiloHome from "../Home/Home.module.css";
 
 const PostPage = () => {
-    const objecto = useLocation();
-
-    useEffect(() => {
-        console.log(objecto.state);
-    }, []);
+    const objecto = useLocation().state;
 
     return (
-        <div id={styles.container}>
-            <p>postPage</p>
+        <div id={estiloHome.container}>
+            <div id={estiloHome.left}>
+                <h2 id={styles.tit}>{objecto.titulo}</h2>
+                <div id={styles.fundo}>
+                    <img src={objecto.imagem} id={styles.img} alt="Imagem de destaque" />
+                    <p id={styles.conteudo} dangerouslySetInnerHTML={{__html: objecto.conteudo}}></p>
+                </div>
+            </div>
+            <div id={estiloHome.right}></div>
         </div>
     );
 };
