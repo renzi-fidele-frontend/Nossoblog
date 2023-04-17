ad161002d1f3f44345a9bff59ea83ae801add6f9
Renzi Fidele <renzifidele2001@gmail.com>
2023-04-18 01:42:38 +0200

    Acançando

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,21 @@
 import styles from "./Dashboard.module.css"
+import hand from "../../Images/empty-hand.svg"
 
 const Dashboard = () => {
+  
+
+
+
+
   return (
-    <section>Dashboard</section>
+    <section id={styles.container}>
+      <h2>Dashboard</h2>
+      <p>Gerencie os seus Posts</p>
+
+      {/*Caso Hajam posts criados pelo administrador da conta */}
+      {}
+
+    </section>
   )
 }
 
--- src/pages/PostPage/PostPage.jsx
+++ src/pages/PostPage/PostPage.jsx
@@ -1,11 +1,41 @@
-import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import { db } from "../../firebase/config";
 import styles from "./PostPage.module.css";
 import estiloHome from "../Home/Home.module.css";
+import { HiUserCircle } from "react-icons/hi";
+import { useEffect, useState } from "react";
+import {BsCalendar2DateFill} from "react-icons/bs"
+import {AiFillTags} from "react-icons/ai"
 
 const PostPage = () => {
     const objecto = useLocation().state;
+    const [TagsOrg, setTagsOrg] = useState("")
+    const [data, setData] = useState("");
+
+    //  Orgazizando as tags a mostrar
+    function organizar(tags) {
+        var frase = "";
+        tags.forEach((v) => {
+            frase += `#${v} `;
+        });
+        setTagsOrg(frase);
+    }
+
+    //  Convertendo o tempo em segundos para formato de data
+    function toDateTime(secs) {
+        let t = new Date(1970, 0, 1); // Epoch
+        t.setSeconds(secs);
+        setData(t);
+        let dd = t.getDate();
+        let mm = t.getMonth();
+        let yyyy = t.getFullYear();
+        let frase = `${dd}/${mm}/${yyyy}`;
+        setData(frase);
+    }
+
+    useEffect(()=> {
+        organizar(objecto.tags)
+        toDateTime(objecto.criadoEm.seconds)
+    }, [objecto])
 
     return (
         <div id={estiloHome.container}>
@@ -13,7 +43,26 @@ const PostPage = () => {
                 <h2 id={styles.tit}>{objecto.titulo}</h2>
                 <div id={styles.fundo}>
                     <img src={objecto.imagem} id={styles.img} alt="Imagem de destaque" />
-                    <p id={styles.conteudo} dangerouslySetInnerHTML={{__html: objecto.conteudo}}></p>
+
+                    <div id={styles.line}>
+                        <div>
+                            <HiUserCircle size={23} />
+
+                            <p>{objecto.criadoPor}</p>
+                        </div>
+                        <div>
+                            <BsCalendar2DateFill size={23} />
+
+                            <p>{data}</p>
+                        </div>
+                        <div>
+                            <AiFillTags size={23} />
+
+                            <p>{TagsOrg}</p>
+                        </div>
+                    </div>
+
+                    <p id={styles.conteudo} dangerouslySetInnerHTML={{ __html: objecto.conteudo }}></p>
                 </div>
             </div>
             <div id={estiloHome.right}></div>
