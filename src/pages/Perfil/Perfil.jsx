5a900f02ea57de539868e3c277ced7a1a2c150c2
Renzi Fidele <renzifidele2001@gmail.com>
2024-02-05 06:08:01 +0200

    Avançando com a pagina do perfil

--- src/pages/Perfil/Perfil.jsx
+++ src/pages/Perfil/Perfil.jsx
@@ -2,19 +2,23 @@ import React, { useRef, useState } from "react";
 import styles from "./Perfil.module.css";
 import { useSelector } from "react-redux";
 import fotoDemo from "../../Images/me.png";
+import semFotoPerfil from "/src/Images/noprofile.jpg";
 
 // Icons
 import { FaUserEdit } from "react-icons/fa";
+import { updateProfile } from "firebase/auth";
+import { v4 } from "uuid";
+import { getDownloadURL, ref } from "firebase/storage";
+import { storage } from "../../firebase/config";
+import checkIfImageExists from "../../hooks/useCheckIfImageExists";
 
 const Perfil = () => {
    const { user } = useSelector((state) => state.user);
    const nome_ref = useRef(null);
    const email_ref = useRef(null);
    const [alterado, setAlterado] = useState(false);
-
-   function carregarFoto() {
-      return; //  TODO
-   }
+   const [fotoPerfil, setFotoPerfil] = useState(null);
+   const [linkFoto, setLinkFoto] = useState("");
 
    function removerFoto() {
       return; //  TODO
@@ -29,6 +33,40 @@ const Perfil = () => {
       }
    }
 
+   //  Adicionando a imagem ao DB
+   async function uploadImagem() {
+      const imageRef = ref(storage, `fotosPerfil/${fotoPerfil.name + v4()}`);
+      uploadBytes(imageRef, fotoPerfil).then((v) => {
+         getDownloadURL(v.ref).then(async (link) => {
+            setLinkFoto(link);
+         });
+      });
+   }
+
+   async function atualizarPerfil(e, comImagem = false) {
+      e.preventDefault();
+      if (comImagem) {
+         if (fotoPerfil.length > 0) {
+            uploadImagem();
+            await updateProfile(user, { displayName: nome_ref.current.value, photoURL: linkFoto })
+               .then(() => {
+                  console.log("Perfil atualizado com sucesso!");
+               })
+               .catch((err) => {
+                  console.log(err.code, err.message);
+               });
+         }
+      } else {
+         await updateProfile(user, { displayName: nome_ref.current.value })
+            .then(() => {
+               console.log("Perfil atualizado com sucesso!");
+            })
+            .catch((err) => {
+               console.log(err.code, err.message);
+            });
+      }
+   }
+
    return (
       <div id={styles.ct}>
          <i>
@@ -39,17 +77,33 @@ const Perfil = () => {
          <div id={styles.duasCol}>
             <div id={styles.left}>
                <figure>
-                  <img src={fotoDemo} alt="Foto de perfil do usuário" />
+                  {linkFoto.length > 0 ? (
+                     <img src={fotoDemo} alt="Foto de perfil do usuário" />
+                  ) : (
+                     <img src={semFotoPerfil} alt="Foto de perfil do usuário" />
+                  )}
                </figure>
             </div>
             <div id={styles.right}>
-               <button id={styles.loadBtn}>Carregar nova foto</button>
+               <input
+                  hidden
+                  type="file"
+                  id="fotoPerfil"
+                  accept="image/*"
+                  onChange={(e) => {
+                     console.log(`Foto carregada: ${e.target.files[0]}`);
+                     setFotoPerfil(e.target.files[0]);
+                  }}
+               />
+               <label for="fotoPerfil" id={styles.loadBtn}>
+                  Carregar nova foto
+               </label>
                <button id={styles.removeBtn}>Remover foto</button>
             </div>
          </div>
-         <form autoComplete={false} id={styles.form}>
+         <form onSubmit={atualizarPerfil} autoComplete={false} id={styles.form}>
             <fieldset>
-               <label htmlFor="">Nome do usuário:</label>
+               <label>Nome do usuário:</label>
                <input
                   onChange={handleChange}
                   defaultValue={user.displayName}
@@ -60,7 +114,7 @@ const Perfil = () => {
                />
             </fieldset>
             <fieldset>
-               <label htmlFor="">Email:</label>
+               <label>Email:</label>
                <input
                   onChange={handleChange}
                   defaultValue={user.email}
@@ -71,7 +125,7 @@ const Perfil = () => {
                />
             </fieldset>
 
-            <button style={{ opacity: !alterado && "0.5", transition: "all 0.3s ease-out" }} disabled={!alterado}>
+            <button type="submit" style={{ opacity: !alterado && "0.5", transition: "all 0.3s ease-out" }} disabled={!alterado}>
                Salvar
             </button>
          </form>
