118a68a51c1f7ebe0003d8da198fc9b0cab197b9
renzi-fidele-frontend <renzifidele2001@gmail.com>
2024-02-19 02:45:35 +0200

    Avançando com a estrutura do chat

--- src/Components/Conversa/Conversa.jsx
+++ src/Components/Conversa/Conversa.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "./Conversa.module.css";
 import { useSelector } from "react-redux";
+import SmoothScrollbar from "smooth-scrollbar";
 
 // React Icons
 import { AiOutlinePicture, AiOutlineUpload } from "react-icons/ai";
 import { IoSend } from "react-icons/io5";
+import foto from "../../Images/fotoMSG.jpg"
 
 const Conversa = () => {
    const { user } = useSelector((state) => state.user);
+   const scrollRef = useRef(null);
+
+   useEffect(() => {
+      console.log(scrollRef.current);
+      SmoothScrollbar.init(scrollRef.current);
+   }, [scrollRef]);
 
    return (
       <div id={styles.ct}>
@@ -15,20 +23,37 @@ const Conversa = () => {
             <h5>Nome completo</h5>
             <p>Estado online</p>
          </div>
-         <div id={styles.chatBody}>
+         <div ref={scrollRef} id={styles.chatBody}>
             <div id={styles.mensagens}>
                {[1, 2, 3, 4, 5].map((v, k) => (
                   <div key={k} className={styles.msg} id={styles.recebido}>
-                     <img src={user?.photoURL} alt="Foto do usuário" />
-                     <p>Texto da mensagem</p>
+                     <div>
+                        <img className={styles.fotoUser} src={user?.photoURL} alt="Foto do usuário" />
+                        <span>08:30</span>
+                     </div>
+                     <div className={styles.conteudoMsg}>Texto da mensagem</div>
                   </div>
                ))}
                {[1, 2, 4].map((v, k) => (
                   <div key={k} className={styles.msg} id={styles.enviado}>
-                     <img src={user?.photoURL} alt="Foto do usuário" />
-                     <p>Texto da mensagem</p>
+                     <div>
+                        <img className={styles.fotoUser} src={user?.photoURL} alt="Foto do usuário" />
+                        <span>08:30</span>
+                     </div>
+                     <div className={styles.conteudoMsg}>Texto da mensagem</div>
                   </div>
                ))}
+
+               {/*Modelo de mesagem contendo imagem */}
+               <div className={styles.msg} id={styles.enviado}>
+                  <div>
+                     <img className={styles.fotoUser} src={user?.photoURL} alt="Foto do usuário" />
+                     <span>08:30</span>
+                  </div>
+                  <div className={styles.conteudoMsg}>
+                     <img src={foto} alt="" />
+                  </div>
+               </div>
             </div>
          </div>
          <form id={styles.input}>
